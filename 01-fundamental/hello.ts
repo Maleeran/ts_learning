c5b21da593a574861a1383fe5b20abad26bc3b21
Liangbo Gao <15803871761@163.com>
2026-01-14 20:26:19 +0800

    1.14 save

---
+++ 01-fundamental/hello.ts
@@ -0,0 +1,20 @@
+// "use strict";
+// Greets the world.
+// console.log("Hello world!");
+
+// function greet(person: string, date: Date) {
+//   console.log(`Hello ${person}, today is ${date.toDateString()}!`);
+// }
+
+// greet("Maleeran", new Date());
+
+// Everyday Types
+
+// The primitives: string, number, and boolean
+
+let myName: string;
+myName = "Maleeran";
+let age: number;
+age = 25;
+let isStudent: boolean;
+isStudent = true;
