a90684d7b5de572d26839a443a7f0cba2154d48a
Liangbo Gao <15803871761@163.com>
2026-01-23 22:17:40 +0800

    1.22 save

--- 02-Narrowing/Narrowing.ts
+++ 02-Narrowing/Narrowing.ts
@@ -12,4 +12,39 @@ function padLeft(padding: number | string, input: string) {
     // check type : type guard in TS
     return new Array(padding + 1).join(" ") + input;
   }
+
+  return padding + input;
+}
+
+// typeof: type guard
+
+function printAll(strs: string | string[] | null) {
+  if (typeof strs === "object") {
+    for (const s of strs) {
+      // Object is possibly 'null'.
+      console.log(s);
+    }
+  } else if (typeof strs === "string") {
+    console.log(strs);
+  } else {
+    // do nothing
+  }
+}
+
+printAll(["Dog", "Cat"]);
+printAll(null);
+
+// Truthiness narrowing
+// in js, it always return the type of the value to boolean.
+console.log(Boolean("hello"));
+console.log(Boolean(0));
+
+function printAll2(strs: string | string[] | null) {
+  if (strs && typeof strs === "object") {
+    for (const s of strs) {
+      console.log(s);
+    }
+  } else if (typeof strs === "string") {
+    console.log(strs);
+  }
 }
