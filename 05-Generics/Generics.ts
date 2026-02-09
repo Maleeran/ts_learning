3d3236c68e0fedb671fbfac24327e50921691a03
Liangbo Gao <15803871761@163.com>
2026-02-09 22:15:06 +0800

    2.9 save

--- 05-Generics/Generics.ts
+++ 05-Generics/Generics.ts
@@ -57,4 +57,51 @@ class GenericNumber<NumType> {
   }
 }
 
-//
+// Generic Constraints
+interface Lengthwise {
+  length: number;
+}
+
+function loggingIdentity<Type extends Lengthwise>(arg: Type): Type {
+  console.log(arg.length); // Now we know it has a .length property, so no more error
+  return arg;
+}
+
+// using Type Parameters
+function getProperty<Type, Key extends keyof Type>(obj: Type, key: Key) {
+  return obj[key];
+}
+
+let x = { a: 1, b: 2, c: 3, d: 4 };
+
+getProperty(x, "a");
+// getProperty(x, "m"); // Argument of type '"m"' is not assignable to parameter of type '"a" | "b" | "c" | "d"'.
+
+// Using Class Types in Generics
+
+class DogKeeper {
+  behavior: string = "playing plate";
+}
+
+class CatKeeper {
+  behavior: string = "playing hairball";
+}
+
+class Pets {
+  legs: number = 4;
+}
+
+class Dogs extends Pets {
+  keeper: DogKeeper = new DogKeeper();
+}
+
+class Cats extends Pets {
+  keeper: CatKeeper = new CatKeeper();
+}
+
+function createInstance<A extends Pets>(c: new () => A): A {
+  return new c();
+}
+
+const dog = createInstance(Dogs);
+console.log(dog);
