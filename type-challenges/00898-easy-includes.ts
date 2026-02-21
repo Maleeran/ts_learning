ea3e36b7fdd0668d8e1d27bb350899c1ba057567
Liangbo Gao <15803871761@163.com>
2026-02-21 23:26:19 +0800

    2.21 Update

--- type-challenges/00898-easy-includes.ts
+++ type-challenges/00898-easy-includes.ts
@@ -31,7 +31,7 @@ type Includes<T extends readonly any[], U> = T extends [
   infer First,
   ...infer Rest,
 ]
-  ? MyEqual<First, U> extends true
+  ? Equal<First, U> extends true
     ? true
     : Includes<Rest, U>
   : false;
